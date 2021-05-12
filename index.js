da9c05e182897f57b24a415e07f79792c5c58736
Weston Pace <weston.pace@gmail.com>
2021-05-11 15:41:10 -1000

    WIP

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ try {
   const myToken = core.getInput('token');
   const octokit = github.getOctokit(myToken);
   const payload = JSON.stringify(github.context.payload, undefined, 2)
+
+  prNumber = github.context.payload.number;
+
+  await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments', {
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    issue_number: prNumber,
+    body: 'Hello!  This is a sample comment created by a bot'
+  });
+
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
