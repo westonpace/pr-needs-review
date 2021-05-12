2b3e0e63bb7d67a1b3c4422b9b28fbaa3d61118e
Weston Pace <weston.pace@gmail.com>
2021-05-11 15:44:05 -1000

    WIP

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  const myToken = core.getInput('token');
-  const octokit = github.getOctokit(myToken);
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-
-  prNumber = github.context.payload.number;
-
-  await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments', {
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    issue_number: prNumber,
-    body: 'Hello!  This is a sample comment created by a bot'
-  });
-
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+async function run() {
+    try {
+        const myToken = core.getInput('token');
+        const octokit = github.getOctokit(myToken);
+        const payload = JSON.stringify(github.context.payload, undefined, 2)
+      
+        prNumber = github.context.payload.number;
+      
+        await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments', {
+          owner: github.context.repo.owner,
+          repo: github.context.repo.repo,
+          issue_number: prNumber,
+          body: 'Hello!  This is a sample comment created by a bot'
+        });
+      
+        console.log(`The event payload: ${payload}`);
+      } catch (error) {
+        core.setFailed(error.message);
+      }
 }
+
+run().catch(err => {
+    core.setFailed(err.message);
+});
+
